265ee445fa326ed707127184f4562c8724187908
Gianmaria Canossa <gianmaria.canossa@gmail.com>
2024-04-03 10:35:12 +0200

    feat: disposable scope/handle

---
+++ GC.Blazor/DisposableHandle.cs
@@ -0,0 +1,31 @@
+﻿
+namespace GC.Blazor;
+
+public class DisposableHandle : IAsyncDisposable, IDisposable
+{
+  private readonly IDisposable? _disposable;
+  private readonly IAsyncDisposable? _asyncDisposable;
+
+  public DisposableHandle(IDisposable item)
+  {
+    _disposable = item;
+  }
+  public DisposableHandle(IAsyncDisposable item)
+  {
+    _asyncDisposable = item;
+  }
+
+  public void Dispose()
+  {
+    System.GC.SuppressFinalize(this);
+
+    _disposable?.Dispose();
+  }
+
+  public async ValueTask DisposeAsync()
+  {
+    Dispose();
+
+    if(_asyncDisposable != null) await _asyncDisposable.DisposeAsync();
+  }
+}
---
+++ GC.Blazor/WebApi.cs
@@ -0,0 +1,17 @@
+﻿namespace GC.Blazor;
+
+public static class WebApi
+{
+  public static IDisposable SetTimeout(Action callback, TimeSpan timeout)
+  {
+    var timer = new Timer(p => callback?.Invoke(), null, (int)timeout.TotalMicroseconds, Timeout.Infinite);
+
+    return new DisposableHandle((IDisposable)timer);
+  }
+  public static IDisposable SetInterval(Action callback, TimeSpan timeout)
+  {
+    var timer = new Timer(p => callback?.Invoke(), null, (int)timeout.TotalMicroseconds, (int)timeout.TotalMicroseconds);
+
+    return new DisposableHandle((IDisposable)timer);
+  }
+}
