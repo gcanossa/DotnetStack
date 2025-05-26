a868508a86b78574c7f153549b25dce26c49f338
Gianmaria Canossa <gianmaria.canossa@gmail.com>
2025-05-26 17:51:15 +0200

    feat: ResizeMode and ObjectUrl support

--- GKit.BlazorExt/DownloadFileService.cs
+++ GKit.BlazorExt/DownloadFileService.cs
@@ -16,6 +16,22 @@ public class DownloadFileService(IJSRuntime jsRuntime) : IAsyncDisposable
     await module.InvokeVoidAsync("downloadFileFromStream", fileName, streamRef);
   }
 
+  public async Task<string> CreateObjectURLFromStream(Stream source, string? contentType = null)
+  {
+    var module = await moduleTask.Value;
+
+    using var streamRef = new DotNetStreamReference(stream: source);
+
+    return await module.InvokeAsync<string>("createObjectURLFromStream", streamRef, contentType);
+  }
+
+  public async Task RevokeObjectURL(string url)
+  {
+    var module = await moduleTask.Value;
+
+    await module.InvokeVoidAsync("revokeObjectURL", url);
+  }
+
   public async ValueTask DisposeAsync()
   {
     if (moduleTask.IsValueCreated)
--- GKit.BlazorExt/wwwroot/DownloadFileService.js
+++ GKit.BlazorExt/wwwroot/DownloadFileService.js
@@ -1,11 +1,24 @@
 export async function downloadFileFromStream(fileName, contentStreamReference) {
-  const arrayBuffer = await contentStreamReference.arrayBuffer();
-  const blob = new Blob([arrayBuffer]);
-  const url = URL.createObjectURL(blob);
+  const url = await createObjectURLFromStream(contentStreamReference);
   const anchorElement = document.createElement("a");
   anchorElement.href = url;
   anchorElement.download = fileName ?? "";
   anchorElement.click();
   anchorElement.remove();
+  await revokeObjectURL(url);
+}
+
+export async function createObjectURLFromStream(
+  contentStreamReference,
+  contentType
+) {
+  const arrayBuffer = await contentStreamReference.arrayBuffer();
+  const blob = new Blob([arrayBuffer], { type: contentType });
+  const url = URL.createObjectURL(blob);
+
+  return url;
+}
+
+export async function revokeObjectURL(url) {
   URL.revokeObjectURL(url);
 }
