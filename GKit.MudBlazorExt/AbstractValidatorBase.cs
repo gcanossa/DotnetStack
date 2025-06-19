67c61c718e1d9568df0feb0f7437cc36ed58e73a
Gianmaria Canossa <gianmaria.canossa@gmail.com>
2025-06-19 23:13:12 +0200

    fix: EditEntityDialog validation feat: updated packages

--- GKit.MudBlazorExt/AbstractValidatorBase.cs
+++ GKit.MudBlazorExt/AbstractValidatorBase.cs
@@ -4,11 +4,11 @@ namespace GKit.MudBlazorExt;
 
 public abstract class AbstractValidatorBase<T> : AbstractValidator<T>
 {
-  public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
-    {
-      var result = await ValidateAsync(ValidationContext<T>.CreateWithOptions((T)model, x => x.IncludeProperties(propertyName)));
-      if (result.IsValid)
-        return [];
-      return result.Errors.Select(e => e.ErrorMessage);
-    };
+  public async Task<IEnumerable<string>> ValidateValueAsync(object model, string propertyName)
+  {
+    var result = await ValidateAsync(ValidationContext<T>.CreateWithOptions((T)model, x => x.IncludeProperties(propertyName)));
+    if (result.IsValid)
+      return [];
+    return result.Errors.Select(e => e.ErrorMessage);
+  }
 }
